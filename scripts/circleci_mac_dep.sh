4f409049a3b22cfcb0a203f93eb8d703c76851ff
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-28 18:21:11 -0700

    need nose

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info
+conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info nose
 pip install dropbox
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install
