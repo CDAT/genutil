e968dc954e12499d2f14c4feef91c86bbea61501
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-29 08:42:11 -0700

    adding uduints2 as dependency

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info nose
+conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose
 pip install dropbox
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install
