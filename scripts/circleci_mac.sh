d3ae0e854c5e845a6f4266cb69ff1dfc48c421c2
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-30 13:59:42 -0700

    forgot to assign result to a variable

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -1,5 +1,6 @@
 export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
 python run_tests.py -v2 
+rc=$?
 if [ ${rc} -ne 0 ]; then exit ${rc} ; fi
 if [ ${rc} -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
