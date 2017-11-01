ba33954ae676298beef56cb55993424fe7bbd6e1
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-01 14:41:53 -0700

    udunuits2 needed

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3"
+conda create -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3" udunits2
 conda create -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py3
