6b58d354ca3ee40fa33f7b391d63116971b83989
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-02 13:25:40 -0700

    activate env and test both py2 and py3 for circleci

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -1,6 +1,11 @@
 export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
+source activate py2
 python run_tests.py -v2 
-rc=$?
-if [ ${rc} -ne 0 ]; then exit ${rc} ; fi
-if [ ${rc} -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
+RESULT=$?
+source activate py3
+python run_tests.py -v2 
+RESULT=$(( $RESULT + $? ))
+echo "RESULT:"${RESULT}
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
+exit $RESULT
