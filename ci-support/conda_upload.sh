94d9078cc0289102f1865d003b60552820dfd109
Charles Doutriaux <doutriaux1@llnl.gov>
2018-02-27 11:48:52 -0800

    newest version of conda building

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -3,15 +3,14 @@ PKG_NAME=genutil
 USER=uvcdat
 export PATH="$HOME/miniconda/bin:$PATH"
 echo "Trying to upload conda"
+ESMF_CHANNEL="nesii/label/dev-esmf"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
     echo "Linux OS"
     conda update -y -q conda
-    ESMF_CHANNEL="nesii/label/dev-esmf"
 else
     echo "Mac OS"
     OS=osx-64
-    ESMF_CHANNEL="nadeau1"
 fi
 
 mkdir ~/conda-bld
@@ -19,22 +18,13 @@ source activate root
 conda install -q anaconda-client conda-build
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
-export VERSION=`date +%Y.%m.%d`
+#export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
-python ./prep_for_build.py -v `date +%Y.%m.%d` 
+python ./prep_for_build.py
 echo "Building and uploading now"
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.13 --python=2.7
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.12 --python=2.7
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.11 --python=2.7
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.10 --python=2.7
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.9 --python=2.7
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.13 --python=3.6
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.12 --python=3.6
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.11 --python=3.6
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.10 --python=3.6
-conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME} --numpy=1.9 --python=3.6
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-*_0.tar.bz2 --force
+conda build -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat/label/nightly -c uvcdat ${PKG_NAME}
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME*.tar.bz2 --force
