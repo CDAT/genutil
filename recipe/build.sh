0f6f10557877c3343ff30c5be4840780d269314f
Charles Doutriaux <doutriaux1@llnl.gov>
2018-10-17 07:11:29 -0700

    added license moved recipe into repo

---
+++ recipe/build.sh
@@ -0,0 +1,12 @@
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+if [[ `uname` == "Linux" ]]; then
+    export LDSHARED_FLAGS="-shared -pthread"
+else
+    export LDSHARED_FLAGS="-bundle -undefined dynamic_lookup"
+fi
+export LDSHARED="$CC $LDSHARED_FLAGS"
+LDSHARED=$LDSHARED python setup.py install
